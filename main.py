1d6f691bc26e7a25ee05afc7c508aae89d72524e
Hayat Tamboli <hayat.tamboli@gmail.com>
2024-04-11 00:52:27 +0530

    added 2 threads

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ import threading
 
 line = "0"
 
-# Define a function to receive Arduino data
 def receive_data_from_arduino():
-    # Your Arduino data receiving logic goes here
     ser = None
     try:
         ser = serial.Serial(port='COM7', baudrate=9600)
@@ -24,14 +22,9 @@ def receive_data_from_arduino():
 
 
 def run_video():
-    
-    # Path to the prerecorded video
     video_path = 'myvideo.mp4'
-
-    # Open the video file
     cap = cv2.VideoCapture(video_path)
 
-    # Check if the video opened successfully
     if not cap.isOpened():
         print("Error: Unable to open video file")
         return
@@ -46,24 +39,23 @@ def run_video():
         # Read a frame from the video
         ret, frame = cap.read()
 
-        # Check if the frame was read successfully
         if not ret:
             break
 
-        # Display the frame
         cv2.imshow('Video', frame)
 
-        # Calculate delay based on speed-up factor
+        # defining the framerate
         delay = int(1000 / (fps * speed_up_factor))
 
         # Wait for key press
         key = cv2.waitKey(delay)
 
-        # Check if the 'Z' key was pressed
+        # Check if the 'Z' key was pressed or if the line is "1"
         if ((key == ord('z'))|(line == "1")):
             speed_up_factor = 5 
         else:
             speed_up_factor = 1
+        # Check if the 'X' key was pressed
         if (key == ord('x')):
             reverse = not reverse
         if key == 27:  # If 'Esc' key was pressed
@@ -71,22 +63,18 @@ def run_video():
         if reverse:
             cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) - 2)
 
-    # Release the video capture object and close all windows
     cap.release()
     cv2.destroyAllWindows()
 
 # if __name__ == "__main__":
 #     main()
 
-# Create two threads, one for each task
+# to run 2 loops at the same time
 arduino_thread = threading.Thread(target=receive_data_from_arduino)
 video_thread = threading.Thread(target=run_video)
 
-# Start both threads
 arduino_thread.start()
 video_thread.start()
 
-
-# Wait for both threads to finish
 arduino_thread.join()
 video_thread.join()
